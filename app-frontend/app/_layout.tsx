f9be2e025107c70ce60caedcbed4383e8725d544
szenafi <87313012+szenafi@users.noreply.github.com>
2025-06-16 20:18:29 +0200

    Remove notification system

--- app-frontend/app/_layout.tsx
+++ app-frontend/app/_layout.tsx
@@ -1,17 +1,12 @@
 // app/_layout.tsx
 // @ts-ignore
 import { AuthProvider } from '../context/AuthContext';
-import { NotificationProvider } from '../context/NotificationContext';
 import { Slot } from 'expo-router';
-import ConsentToast from '../components/notifications/ConsentToast';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <NotificationProvider>
-        <Slot />
-        <ConsentToast />
-      </NotificationProvider>
+      <Slot />
     </AuthProvider>
   );
 }
